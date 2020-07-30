267a71b9bdf329db446d4601dd712dd4d63b3c34
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2020-07-30 16:33:55 +0300

    Примітивно реалізував радіо на сайті за допомогою JS

--- script/radioPlayer.js
+++ script/radioPlayer.js
@@ -1,3 +1,24 @@
 export const radioPlayerInit = () => {
-    console.log("Radio Init");
+    const radio = document.querySelector('.radio');
+    const radioStop = document.querySelector('.radio-stop');
+    const radioCoverImg = document.querySelector('.radio-cover__img');
+    const radioItem = document.querySelectorAll('.radio-item');
+    const radioNavigation = document.querySelector('.radio-navigation');
+    const radioHeader = document.querySelector('.radio-header');
+
+
+    const audio = new Audio();
+    audio.type = 'audio/aac';
+
+    radioStop.disabled = true;
+
+    // реалызація роботи радіо
+    radioNavigation.addEventListener('change', event => {
+        const target = event.target;
+
+        audio.src = target.dataset.radioStantion;
+
+        audio.play();
+    });
+
 };
