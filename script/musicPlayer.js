de6133e04c389df9e2e8f27d5feb08bf9565bb49
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2020-07-30 22:52:17 +0300

    створив перший функіонал аудіоплеєра на сайті за допомогою JS

--- script/musicPlayer.js
+++ script/musicPlayer.js
@@ -1,3 +1,36 @@
 export const musicPlayerInit = () => {
-    console.log("music Init");
+    const audio = document.querySelector('.audio');
+    const audioImg = document.querySelector('.audio-img');
+    const audioHeader = document.querySelector('.audio-header');
+    const audioPlayer = document.querySelector('.audio-player');
+    const audioNavigation = document.querySelector('.audio-navigation');
+    const audioButtonPlay = document.querySelector('.audio-button__play');
+    const audioProgress = document.querySelector('.audio-progress');
+    const audioProgressTiming = document.querySelector('.audio-progress__timing');
+    const audioTimePassed = document.querySelector('.audio-time__passed');
+    const audioTimeTotal = document.querySelector('.audio-time__total');
+
+    const playList = ['hello', 'flow', 'speed']; // масив з музикою
+
+    let trackIndex = 0;
+
+
+    audioNavigation.addEventListener('click', event => {
+        const target = event.target;
+
+        if (target.classList.contains('audio-button__play')) {
+            audio.classList.toggle('play'); // аніміція картинки    
+            audioButtonPlay.classList.toggle('fa-play'); // заміна іконки
+            audioButtonPlay.classList.toggle('fa-pause'); //заміна іконки
+
+            if (audioPlayer.paused) { // включение музики
+                audioPlayer.play();
+            } else {
+                audioPlayer.pause();
+            }
+
+        }
+    });
+
+
 };
